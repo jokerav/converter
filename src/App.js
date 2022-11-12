08e3b4822e1e24fd7478cf10f0187f3f77556992
jokerav <jokeravdeveloper@gmail.com>
2022-11-12 12:15:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import Form from "./components/Form/form";
 import './App.css';
 import 'antd/dist/antd.css';
-// import { useGetDataQuery} from './store/dataApi';
+import { useGetDataQuery} from './store/dataApi';
+//номера валлют 2,24,25,32
 
 
 function App() {
-  // const { data = []
-  //   // , error, isLoading
-  // } = useGetDataQuery();
-  // console.log(data);
+  const { data = []
+    // , error, isLoading
+  } = useGetDataQuery();
+  const currency = ['USD', 'CNY', "EUR", 'GBP'];
+  let currencyValue = [];
+  data.map(({cc,rate})=>{
+    if (currency.includes(cc)){
+      currencyValue.push({[cc]:rate})
+    }});
+  console.log(currencyValue);
+
 
   return (
       <Form/>
